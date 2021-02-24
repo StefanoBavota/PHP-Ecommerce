2ccc115cdff5746abd9d4f3640829af4b667f82d
StefanoBavota <stefano.bavota@hotmail.com>
2021-02-24 21:29:55 +0100

    add product view only in products-list

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $page = isset($_GET["page"]) ? $_GET["page"] : "dashboard";
 ?>
@@ -6,14 +6,16 @@ $page = isset($_GET["page"]) ? $_GET["page"] : "dashboard";
 <?php include '../inc/init.php' ?>
 <?php include ROOT_PATH . 'public/template-parts/header.php' ?>
 
-<div id = "main" class = "container" style = "margin-top:100px;"> 
-    <div class = "row">
-    
-        <div class = "col-md-9 col-xs-12">
+<div id="main" class="container" style="margin-top:100px;">
+    <div class="row">
+
+        <div class="col-md-9 col-xs-12">
             <?php include ROOT_PATH . 'admin/pages/' . $page . '.php' ?>
         </div>
-    
-        <?php include ROOT_PATH . 'admin/pages/sidebar.php' ?>
+
+        <?php if ($page == 'products-list') : ?>
+            <?php include ROOT_PATH . 'admin/pages/sidebar.php' ?>
+        <?php endif; ?>
 
     </div>
 </div>
