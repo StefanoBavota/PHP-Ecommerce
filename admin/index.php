45c869851210437a640cbcfc7c12a8eaa6eb213c
StefanoBavota <stefano.bavota@hotmail.com>
2021-02-26 23:45:34 +0100

    fix col index admin

--- admin/index.php
+++ admin/index.php
@@ -9,9 +9,15 @@ $page = isset($_GET["page"]) ? $_GET["page"] : "dashboard";
 <div id="main" class="container" style="margin-top:100px;">
     <div class="row">
 
-        <div class="col-md-9 col-xs-12">
-            <?php include ROOT_PATH . 'admin/pages/' . $page . '.php' ?>
-        </div>
+        <?php if ($loggedInUser && $loggedInUser->is_admin && !$page == 'products-list') : ?>
+            <div class="col-10 col-xs-12">
+                <?php include ROOT_PATH . 'admin/pages/' . $page . '.php' ?>
+            </div>
+        <?php else : ?>
+            <div class="col-md-9 col-xs-12">
+                <?php include ROOT_PATH . 'admin/pages/' . $page . '.php' ?>
+            </div>
+        <?php endif; ?>
 
         <?php if ($page == 'products-list') : ?>
             <?php include ROOT_PATH . 'admin/pages/sidebar.php' ?>
