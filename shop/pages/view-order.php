b517c47f7c017a9997d0ac8955ff52e0715c4723
LucaEvangelista <luca10evangelista@gmail.com>
2021-03-02 22:40:12 +0100

    templating view-order

--- shop/pages/view-order.php
+++ shop/pages/view-order.php
@@ -4,6 +4,8 @@ if (!defined('ROOT_URL')) {
     die;
 }
 
+require_once ('../vendor/autoload.php');
+
 if (!$loggedInUser) {
     echo "<script>location.href='" . ROOT_URL . "auth?page=login';</script>";
     exit;
@@ -29,36 +31,15 @@ if (count($orderItems) == 0) {
     exit;
 }
 $count = 0;
+
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+$twig = new \Twig\Environment($loader, [ ]);
+
+echo $twig->render('view-order.html', [
+    'orderItems' => $orderItems,
+    'orderTotal' => $orderTotal,
+    'count' => $count
+]);
+
 ?>
 
-<div class="separate-top">
-    <a href="<?php echo ROOT_URL . 'shop?page=my-orders'; ?>" class="back underline scuro">&laquo; I miei Ordini</a>
-</div>
-
-<h1 cass="mb-4">Ordine #<?php echo esc_html($orderId); ?></h1>
-
-<table class="table table-bordered">
-    <tr>
-        <th class="big-screen">#</th>
-        <th>Prodotto</th>
-        <th>Quantità</th>
-        <th class="big-screen">Prezzo Unitario</th>
-        <th>Prezzo</th>
-    </tr>
-    <?php foreach ($orderItems as $item) : $count++; ?>
-
-        <tr>
-            <td class="big-screen"><?php echo $count; ?></td>
-            <td class="big-screen"><?php echo esc_html($item['product_name']); ?></td>
-            <td><?php echo esc_html($item['quantity']); ?></td>
-            <td><?php echo esc_html($item['single_price']); ?> €</td>
-            <td><?php echo esc_html($item['total_price']); ?> €</td>
-        </tr>
-    <?php endforeach;
-    $count = 0; ?>
-    <tr>
-        <th colspan="100%">
-            <h4 class="inline">Totale <?php echo $orderTotal['total']; ?> €</h4>
-        </th>
-    </tr>
-</table>
